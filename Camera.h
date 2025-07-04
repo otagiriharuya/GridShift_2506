54aa1cb2029664ea54c3d0c3a1872cf5884306d0
otagiriharuya <otagi0326@gmail.com>
2025-07-04 23:43:18 +0900

    Camera.hの整理

--- Camera.h
+++ Camera.h
@@ -4,23 +4,34 @@
 
 
 #include <SDL3/SDL.h>
+#include <algorithm>
 
+// Cameraクラス
+// ゲームの視点を管理プレイヤーなどのオブジェクトを追跡し、画面表示を調整
 class Camera {
 public:
-	Camera(float x, float y, float w, float h, float mapW, float mapH); // コンストラクタ
+	// コンストラクト
+	// カメラの初期位置、カメラのビューポートサイズ
+	Camera(float x, float y, float w, float h);
 
-	void Update(float targetX, float targetY, float targetW, float targetH); // 更新処理
+	// カメラの位置更新し、対象を追跡するようにする
+	// 追跡対象の現在のワールド位置、追跡対象の幅と高さ、マップのは幅と高さ
+	void Update(float targetX, float targetY, float targetW, float targetH, int mapW, int mapH);
 	
-	float GetX() const { return cameraRect_.x; } // カメラのX座標を取得
-	float GetY() const { return cameraRect_.y; } // カメラのY座標を取得
-	float GetW() const { return cameraRect_.w; } // カメラの幅を取得
-	float GetH() const { return cameraRect_.h; } // カメラの高さを取得
+    // カメラの現在のX座標を取得
+    float GetX() const { return x_; }
+    // カメラの現在のY座標を取得
+    float GetY() const { return y_; }
+    // カメラのビューポート幅を取得
+    int GetWidth() const { return width_; }
+    // カメラのビューポート高さを取得
+    int GetHeight() const { return height_; }
 
 private:
-	SDL_FRect cameraRect_; // カメラのビューポート
-	
-	float mapW_; // マップの幅
-	float mapH_; // マップの高さ
+    float x_; // カメラの現在のワールドX座標
+    float y_; // カメラの現在のワールドY座標
+    int width_; // カメラのビューポート幅
+    int height_; // カメラのビューポート高さ
 };
 
 #endif // CAMERA_H
