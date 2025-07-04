c3ba5d1bb08b82c975226f2ed6ab80b76642af92
otagiriharuya <otagi0326@gmail.com>
2025-07-05 00:07:35 +0900

    Block.hの整理

--- Block.h
+++ Block.h
@@ -1,36 +1,48 @@
 #pragma once
+#ifndef BLOCK_H
+#define BLOCK_H 
+
 #include <SDL3/SDL.h>
-#include <string>
 #include "Game.h"
 
 // 前方宣言
-class TextureManager;
 class Camera;
 
+// マップ上に配置されるおセルブロックオブジェクトを管理
 class Block {
 public:
 	// コンストラクタ
-	Block(int gridX, int gridY, TextureManager* textureManager, const std::string& texturePath);
+	// ブロックの描画に使用するSDLテクスチャ、マップ上のブロックの初期グリッド座標
+	Block(SDL_Texture* texture, int startGridx, int startGridy);
 	// デストラクト
 	~Block();
 
-	void Render(SDL_Renderer* renderer, Camera* camera);
+    // 更新処理
+    void Update(float deltaTime);
+    // 描画処理
+    // 描画に使用するSDLレンダラー、カメラオブジェクト
+    void Render(SDL_Renderer* renderer, Camera* camera);
 
-	// 現在のグリッド座標取得
-	int GetGridX() const { return gridx_; }
-	int GetGridY() const { return gridy_; }
+    // ブロックの現在のグリッドX座標を取得
+    int GetGridX() const { return gridX_; }
+    // ブロックの現在のグリッドY座標を取得
+    int GetGridY() const { return gridY_; }
 
-	// ピクセル座標取得
-	SDL_FRect GetDestRect() const { return destRect_; }
+    // ブロックのグリッド座標を設定
+    // ブロックの新しいグリッド座標
+    void SetGridPosition(int newGridX, int newGridY);
 
-	// グリッド座標更新関数
-	void SetGridPosition(int newGridX, int newGridY);
+    // ブロックのピクセル座標（X軸）を取得
+    float GetX() const { return destRect_.x; }
+    // ブロックのピクセル座標（Y軸）を取得
+    float GetY() const { return destRect_.y; }
 
 private:
-	SDL_Texture* texture_;
-	SDL_FRect destRect_;
+    SDL_Texture* texture_; // ブロックの描画に使用するテクスチャ
+    SDL_FRect destRect_; // ブロックの画面上の位置とサイズ
 
-	int gridx_; // グリッド座標X
-	int gridy_; // グリッド座標Y
-
+    int gridX_; // ブロックが現在いるグリッドのX座標
+    int gridY_; // ブロックが現在いるグリッドのY座標
 };
+
+#endif // BLOCK_H
